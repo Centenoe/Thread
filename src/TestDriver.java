e547ad4be4ca6efd3d1b32a989e10dd4b3bd2850
jmatte123 <joseph1matteson@gmail.com>
2017-10-25 10:09:44 -0500

    Deleting TestDriver
    It was just a test so I could figure something out.

--- src/TestDriver.java
+++
@@ -1,8 +0,0 @@
-public class TestDriver {
-    public static void main(String[] args) {
-        int[] array = { 5, 6, 3, 6, 3, 5, 1 ,7, 2, 4, 7, 2, 5};
-        if ((array.length % 2) == 1) {
-            System.out.println(array.length / 2);
-        }
-    }
-}
